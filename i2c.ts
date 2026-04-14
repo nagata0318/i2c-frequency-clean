0e1f367c6b294133134efa8b8789a964bb0cc71b
nagata0318 <y-nagata@duplo-seiko.co.jp>
2026-04-14 15:04:26 +0900

    Update i2c.ts

--- i2c.ts
+++ i2c.ts
@@ -1,4 +1,7 @@
 //% weight=100 color=#0fbc11 icon="\uf1eb"
+//% weight=100 color=#0fbc11 icon="\uf1eb"
+//% weight=100 color=#0fbc11 icon="\uf1eb"
+//% weight=100 color=#0fbc11 icon="\uf1eb"
 namespace I2CEx {
 
     //% block="I2C周波数を %hz Hz に設定"
