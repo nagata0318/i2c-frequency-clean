ddd13f669e0cca120747b328285c272c4469529e
nagata0318 <y-nagata@duplo-seiko.co.jp>
2026-04-15 12:09:58 +0900

    Update i2c.cpp

--- i2c.cpp
+++ i2c.cpp
@@ -2,10 +2,8 @@
 
 namespace I2CEx {
 
-    // externで公開（これが無いとリンクされない）
     void _setFrequency(int freq) {
 
-        // micro:bit v1 / v2 両対応
         #if MICROBIT_CODAL
             uBit.i2c.setFrequency(freq);
         #else
