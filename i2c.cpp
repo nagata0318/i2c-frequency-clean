0ac28b21cd25f451415181b4d20565830e7b07f1
nagata0318 <y-nagata@duplo-seiko.co.jp>
2026-04-15 12:02:19 +0900

    Update i2c.cpp

--- i2c.cpp
+++ i2c.cpp
@@ -1,22 +1,10 @@
 #include "pxt.h"
-#include "MicroBit.h"
 
 namespace I2CEx {
 
-    void _setFrequency(int hz) {
-
-        // 周波数制限（安全側）
-        if (hz <= 100000) {
-            hz = 100000;
-        } else if (hz <= 400000) {
-            hz = 400000;
-        } else {
-            hz = 400000;
-        }
-
-        // CODAL I2Cへ直接アクセス
-        if (uBit.i2c.setFrequency) {
-            uBit.i2c.setFrequency(hz);
-        }
+    //% 
+    void _setFrequency(int freq) {
+        // micro:bit runtimeのI2Cインスタンス取得
+        uBit.i2c.setFrequency(freq);
     }
 }
